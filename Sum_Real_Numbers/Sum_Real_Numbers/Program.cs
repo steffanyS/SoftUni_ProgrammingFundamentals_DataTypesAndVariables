4736af3a8a1e36d30abfad9c3b491a01f69e0696
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 19:56:19 +0200

    ex 3 done

--- Sum_Real_Numbers/Sum_Real_Numbers/Program.cs
+++ Sum_Real_Numbers/Sum_Real_Numbers/Program.cs
@@ -10,6 +10,16 @@ namespace Sum_Real_Numbers
     {
         static void Main(string[] args)
         {
+            //Write program to enter n numbers and calculate and print their exact sum (without rounding).
+
+            decimal n = decimal.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for(int i=0; i<n; i++)
+            {
+                sum+= decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
         }
     }
 }
