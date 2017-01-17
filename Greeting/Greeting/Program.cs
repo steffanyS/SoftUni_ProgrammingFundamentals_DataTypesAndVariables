c717e561e88f63554bc5e6ad2a061e19ead1c3bd
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 20:44:35 +0200

    7th ex done

--- Greeting/Greeting/Program.cs
+++ Greeting/Greeting/Program.cs
@@ -10,6 +10,14 @@ namespace Greeting
     {
         static void Main(string[] args)
         {
+            //Write a program that enters first name, last name and age and prints 
+            //"Hello, <first name> <last name>. You are <age> years old."
+
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0} {1}. You are {2} years old.",firstName,lastName,age);
         }
     }
 }
