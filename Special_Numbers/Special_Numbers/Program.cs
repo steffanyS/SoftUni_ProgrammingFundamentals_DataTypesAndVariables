09703fd991f9a0978aa29fd76ab9134871de41e5
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 20:25:09 +0200

    5th exercise done

--- Special_Numbers/Special_Numbers/Program.cs
+++ Special_Numbers/Special_Numbers/Program.cs
@@ -10,6 +10,33 @@ namespace Special_Numbers
     {
         static void Main(string[] args)
         {
+            //A number is special when its sum of digits is 5, 7 or 11.
+            //Write a program to read an integer n and for all numbers in the range 1…n 
+            //to print the number and if it is special or not (True / False).
+
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int num;
+
+            for (int i = 1; i<=n; i++)
+            {
+                num = i;
+                while(num>0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine(i+" -> True");
+                }
+                else
+                {
+                    Console.WriteLine(i + " -> False");
+                }
+                sum = 0;
+            }
         }
     }
 }
