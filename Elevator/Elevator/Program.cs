cdf03cfd54f8917f07873e831fd2d7bd2018997c
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 20:02:03 +0200

    4ex done

---
+++ Elevator/Elevator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculate how many courses will be needed to elevate n persons by using an elevator of capacity of p persons. 
+
+            int n = int.Parse(Console.ReadLine());
+
+            int p = int.Parse(Console.ReadLine());
+
+            int courses=0;
+
+            if(n%p==0)
+            {
+                courses = n / p;
+            }
+            else
+            {
+                courses = n / p+1;
+            }
+
+            Console.WriteLine(courses);
+
+        }
+    }
+}
