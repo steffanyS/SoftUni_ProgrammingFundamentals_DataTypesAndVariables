0996437b91f9e480d87b8949fa334e2152dc0c71
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 19:44:12 +0200

    first exercises

---
+++ Centuries_to_Minutes/Centuries_to_Minutes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centuries_to_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write program to enter an integer number of centuries and convert it to years, days, hours and minutes.
+
+           int centuries = int.Parse(Console.ReadLine());
+
+            double years = centuries * 100;
+
+            double days = Math.Round(years * 365.2422);
+
+            double hours =Math.Round( days * 24);
+
+            double minutes = Math.Round(hours * 60);
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes",
+                                                        centuries, years, (int)days, (int)hours, (int)minutes);
+
+        }
+    }
+}
