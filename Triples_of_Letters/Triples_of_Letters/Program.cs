de7f272fb9a858f43fb660432a896706751398b9
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 20:37:57 +0200

    6th ex done

--- Triples_of_Letters/Triples_of_Letters/Program.cs
+++ Triples_of_Letters/Triples_of_Letters/Program.cs
@@ -10,6 +10,29 @@ namespace Triples_of_Letters
     {
         static void Main(string[] args)
         {
+            //Write a program to read an integer n 
+            //and print all triples of the first n small Latin letters, ordered alphabetically
+
+            int n = int.Parse(Console.ReadLine());
+            char letter;
+
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    for(int k=0; k<n; k++)
+                    {
+                        letter = (char)('a' + i);
+                        Console.Write(letter);
+                        letter = (char)('a' + j);
+                        Console.Write(letter);
+                        letter = (char)('a' + k);
+                        Console.Write(letter);
+                        Console.WriteLine();
+                    }
+                    
+                }
+            }
         }
     }
 }
