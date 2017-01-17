e2d63eeb6f1d841c503df472243441cf36b848b2
steffanyS <stefani.sabcheva@gmail.com>
2017-01-17 20:54:34 +0200

    8th ex

---
+++ Volume_of_Pyramid/Volume_of_Pyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Length: ");
+
+            double lenght = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+
+            double wight = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+
+            double  height = double.Parse(Console.ReadLine());
+            double V = (lenght * wight * height) /3;
+
+            Console.WriteLine("Pyramid Volume: {0:F2}", V);
+
+        }
+    }
+}
